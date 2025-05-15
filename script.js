b3e76c0ebbf056818831978a9d268851c8d44011
Selen Karakaya <122388950+selenkarakaya@users.noreply.github.com>
2025-05-15 16:11:46 +0100

    update

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ const generateMessage = () => {
     .then((data) => {
       // print data for control
       console.log(data);
-
       // invoke randomElement for generate subjects, verbs and endings
       const subjects = randomElement(data.subjects);
       const verbs = randomElement(data.verbs);
@@ -29,3 +28,40 @@ const randomElement = (array) => {
 
 generateMessage();
 button.addEventListener("click", generateMessage);
+
+//  Add animation effects to the button.
+document.addEventListener("DOMContentLoaded", () => {
+  button.addEventListener("click", (e) => {
+    // These give the X and Y coordinates of where the user clicked, relative to the entire page.
+
+    const x = e.pageX;
+    const y = e.pageY;
+
+    //The button's position on the page.
+    const buttonTop = e.currentTarget.offsetTop;
+    const buttonLeft = e.currentTarget.offsetLeft;
+
+    // Buton içindeki tıklama koordinatları
+    const xInside = x - buttonLeft;
+    const yInside = y - buttonTop;
+
+    //These calculate the position inside the button where the user clicked.
+    const circle = document.createElement("span");
+    circle.classList.add("circle");
+    circle.style.top = yInside + "px";
+    circle.style.left = xInside + "px";
+
+    // Append the ripple effect span to the button
+
+    e.currentTarget.appendChild(circle);
+
+    /*  
+If the circle stays in the DOM after the animation:
+A new one is added every time you click, and the old ones pile up.
+This can decrease performance and also mess up the visual appearance.
+ */
+    setTimeout(() => {
+      circle.remove();
+    }, 600);
+  });
+});
